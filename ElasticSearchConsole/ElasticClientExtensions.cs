a44cc8e9e0f5539645abfc499f766af216d7b971
Roman Ogoreltcev <Roman_Ogoreltcev@epam.com>
2017-03-09 14:47:45 +0400

    init + realization put, search, delete

---
+++ ElasticSearchConsole/ElasticClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Elasticsearch.Net;
+using Nest;
+
+namespace ElasticSearchConsole
+{
+    public static class ElasticClientExtensions
+    {
+        public static IIndexResponse Put(this ElasticClient client, string index, string type, int id, object obj)
+        {
+            return client.Index(obj, i => i
+                .Index(index)
+                .Type(type)
+                .Id(id)
+                .Refresh(Refresh.True));
+        }
+
+        public static ISearchResponse<DocumentAttributes> Search(this ElasticClient client, string index, string type, string field, object value)
+        {
+            return client.Search<DocumentAttributes>(s => s
+              .Index(index)
+              .Type(type)
+              .Query(q => q.Term(t => t.Field(field).Value(value))));            
+        }
+
+        public static IDeleteResponse Delete(this ElasticClient client, string index, string type, int id)
+        {
+            return client.Delete<DocumentAttributes>(id, d => d
+                  .Index(index)
+                  .Type(type).Refresh(Refresh.True));
+        }
+
+    }
+}
---
+++ ElasticSearchConsole/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Nest;
+
+namespace ElasticSearchConsole
+{
+    class Program
+    {
+        static void Main()
+        {
+            // создаём клиент
+            var client = GetClient();
+
+            // добавить запись
+            var myJson = new DocumentAttributes
+            {
+                Name = "pig",
+                OriginalVoiceActor = "master",
+                AnimatedDebut = "Smith"
+            };
+            var indexResponse = client.Put("disney", "character", 1, myJson);
+            Console.WriteLine(indexResponse);
+
+            // найти запись по полю id
+            SearchCharacterById(client, 1);
+            SearchCharacterById(client, 2);
+
+            //удалить по id
+            var deleteResponse = client.Delete("disney", "character", 1);
+            Console.WriteLine(deleteResponse);
+
+            // найти запись по полю id
+            SearchCharacterById(client, 1);
+            SearchCharacterById(client, 2);
+        }
+
+        private static void SearchCharacterById(ElasticClient client, int id)
+        {
+           
+            var searchResponse = client.Search("disney", "character", "_id", id);
+            foreach (var hit in searchResponse.Hits)
+            {
+                Console.WriteLine($"id:{hit.Id}\n Source:{hit.Source}");
+            }
+        } 
+
+        private static ElasticClient GetClient()
+        {
+            return new ElasticClient();
+        }
+    }
+
+    public class DocumentAttributes
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string OriginalVoiceActor { get; set; }
+        public string AnimatedDebut { get; set; }
+
+        public override string ToString()
+        {
+            return $"id: {Id}, name: {Name}, original_voice_actor: {OriginalVoiceActor}, animated_debut: {AnimatedDebut}";
+        }
+    }
+}
